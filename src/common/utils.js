800714ed60402d46cbb3c9662a292ff713dcb2bf
hellosibun <sibun.soumya@gmail.com>
2021-05-26 21:10:24 +0530

    added access-token

--- src/common/utils.js
+++
@@ -1,4 +0,0 @@
-export const constants = {
-    userInfoUrl:"https://api.instagram.com/v1/users/self",
-    userMediaUrl:"https://api.instagram.com/v1/users/self/media/recent"
-  }
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,103 +1,90 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import { Button, Card, Grid, TextField, Typography } from "@material-ui/core";
 import Header from "../../common/header/Header";
-import './Login.css';
-import Card from "@material-ui/core/Card";
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import { Redirect } from 'react-router-dom';
+import { access_token } from "../../common";
+import "./Login.css";
 
-class Login extends Component {
+const Login = props => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setErrMsg] = useState({
+    isError: false,
+    msg: ""
+  });
 
-    constructor() {
-        super();
-        this.state = {
-            usernameRequired: "dispNone",
-            passwordRequired: "dispNone",
-            incorrectUsernamePasswordMessage: "dispNone",
-            username: "",
-            password: "",
-            isLoggedIn: false,
-        };
+  // Method to validate and login the user 
+  const authenticateUser = () => {
+    if (username === "admin" && password === "admin") {
+      sessionStorage.userAuth = access_token;
+
+      props.history.push("/home");
+    } else if (!username || !password) {
+      setErrMsg({ isError: true });
+    } else {
+      setErrMsg({ isError: false, msg: "Incorrect username and/or password" });
     }
+  };
 
-    /**
-     * Function that handles any changes in the username field and updates state accordingly
-     */
-    inputUsernameChangeHandler = (e) => {
-        this.setState({ username: e.target.value });
-    }
-
-    /**
-     * Function that handles any changes in the password field and updates state accordingly
-     */
-    inputPasswordChangeHandler = (e) => {
-        this.setState({ password: e.target.value });
-    }
-
-    /**
-     * Function that handles what happens when we click the login button
-     */
-    loginClickHandler = () => {
-        //Setting credentials in the login handler
-        let username = "admin";
-        let password = "admin";
-
-        let accessToken = "IGQVJVbWpBUFNDblh4WkMwLTVjNDNDQ3E3NEJwSktiLVhLZAU1ibjV1V1drcDZAsa1R1S0FGU0FnSHRlUDZAfM1JsbnFhYWpWREk4OG03RmhydC1qT2pYdDZANYTh1eGNVaWo0bF9zbWtUNFI3QjFfaURXS0JDeWhycENSa3NB";
-        if (this.state.username === "" || this.state.password === "") {
-            // The usernameRequired and passwordRequired fields are used when we want to store the class to be assigned 
-            this.state.username === "" ? this.setState({ usernameRequired: "dispBlock" }) : this.setState({ usernameRequired: "dispNone" });
-            this.state.password === "" ? this.setState({ passwordRequired: "dispBlock" }) : this.setState({ passwordRequired: "dispNone" });
-            this.setState({ incorrectUsernamePasswordMessage: "dispNone" });
-        } else if (this.state.username === username && this.state.password === password) {
-            // Setting token in session storage
-            sessionStorage.setItem("access-token", accessToken);
-            // Setting state so as to check and route to home page if login is successful. 
-            this.setState({ 
-                isLoggedIn: true,
-            });
-        } else {
-            // In case the username and password are incorrect
-            this.setState({ incorrectUsernamePasswordMessage: "dispBlock" });
-        }
-
-    }
-
-    render() {
-        return (
-            <div>
-                {this.state.isLoggedIn === true ?
-                <Redirect to= "/home"/>
-                :
-                    <div>
-                        <Header></Header>
-                        <Card className="login-card">
-                            <p className="login-header">LOGIN</p>
-                            <FormControl required>
-                                <InputLabel htmlFor="username">Username</InputLabel>
-                                <Input id="username" type="text" username={this.state.username} onChange={this.inputUsernameChangeHandler} value = {this.state.username}/>
-                                <FormHelperText className={this.state.usernameRequired}><span className="red">required</span></FormHelperText>
-                            </FormControl>
-                            <br />
-                            <br />
-                            <FormControl required>
-                                <InputLabel htmlFor="password">Password</InputLabel>
-                                <Input id="password" type="password" password={this.state.password} onChange={this.inputPasswordChangeHandler} value = {this.state.password}/>
-                                <FormHelperText className={this.state.passwordRequired}><span className="red">required</span></FormHelperText>
-                            </FormControl>
-                            <br />
-                            <br />
-                            <FormHelperText className={this.state.incorrectUsernamePasswordMessage}><span className="red" style={{ fontSize: "14px" }}>Incorrect username and/or password</span></FormHelperText>
-                            <br />
-                            <Button variant="contained" color="primary" onClick={this.loginClickHandler} className="login-btn">LOGIN</Button>
-                        </Card>
-                    </div>
-                }
+  return (
+    <>
+      <Header />
+      <Card className="login-card">
+        <Grid container direction="column">
+          <Grid item>
+            <Typography variant="h5" gutterBottom>
+              LOGIN
+            </Typography>
+          </Grid>
+          <Grid item>
+            <TextField
+              className="form-field"
+              required
+              label="Username"
+              value={username}
+              onChange={e => {
+                setUserName(e.currentTarget.value);
+                setErrMsg({ isError: false, msg: "" });
+              }}
+              fullWidth
+            />
+            <div className="error-msg">
+              {error.isError && !username && "required"}
             </div>
-        );
-    }
-}
+          </Grid>
+          <Grid item>
+            <TextField
+              className="form-field"
+              type="password"
+              required
+              label="Password"
+              value={password}
+              onChange={e => {
+                setPassword(e.currentTarget.value);
+                setErrMsg({ isError: false, msg: "" });
+              }}
+              fullWidth
+            />
+            <div className="error-msg">
+              {error.isError && !password && "required"}
+            </div>
+          </Grid>
+          <Grid className="error-msg" item>
+            {error.msg}
+          </Grid>
+          <Grid item>
+            <Button
+              color="primary"
+              className="login-button"
+              variant="contained"
+              onClick={authenticateUser}
+            >
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </>
+  );
+};
 
 export default Login;
